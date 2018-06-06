a48364a74ea0c3770d763ce550f91d6168151c71
david <code4funlnyx@gmail.com>
2018-06-06 11:51:44 +0800

    supoort disable node

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ module.exports = {
             assetNameRegExp: /\.min\.css$/,
         }),
         new webpack.BannerPlugin(
-            'treejs\n@version 1.2.0\n@see https://github.com/daweilv/treejs'
+            'treejs\n@version 1.3.0\n@see https://github.com/daweilv/treejs'
         ),
     ],
 };
